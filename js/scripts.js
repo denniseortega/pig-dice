56cd504affa63b0d94abedbcf15d95221bfb5467
Dennise Ortega and Noah Medoff <noah@>
2018-04-04 09:19:06 -0700

    created player object

--- js/scripts.js
+++ js/scripts.js
@@ -6,10 +6,17 @@ function dice(num){
   this.num = num;
 }
 
-// Prototype Method that changes the dice number 
+// Prototype Method that changes the dice number
 dice.prototype.roll = function() {
   var newNum = Math.floor(Math.random() * 6) + 1 ;
   return this.num = newNum;
 };
+
+function player(score, playerDice){
+  playerDice = new dice();
+  score = 0;
+  this.score=score;
+  this.playerDice = playerDice;
+}
 
 // Front End Logic
