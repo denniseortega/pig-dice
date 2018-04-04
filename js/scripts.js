fe1229dd164f2d9a8e8507864c6b6a08bb7438e5
Dennise Ortega and Noah Medoff <noah@>
2018-04-04 08:59:58 -0700

    create prototype method to roll the dice

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,14 @@
 
 // Dice Object Constructor
 function dice(num){
+  num = 6;
   this.num = num;
 }
 
+// Prototype Method that changes the dice number 
+dice.prototype.roll = function() {
+  var newNum = Math.floor(Math.random() * 6) + 1 ;
+  return this.num = newNum;
+};
 
 // Front End Logic
